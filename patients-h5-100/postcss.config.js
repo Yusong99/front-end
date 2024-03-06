c4ded020f125eb4053311a399ed816b8e35ec780
Yusong99 <yusongxu1999@gmail.com>
2024-03-06 23:49:19 +0800

    2024.3.6 23:49

---
+++ patients-h5-100/postcss.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-undef
+export const plugins = {
+    'postcss-px-to-viewport': {
+        // 设备宽度375计算vw的值
+        viewportWidth: 375,
+    },
+};
--- patients-h5-100/src/main.ts
+++ patients-h5-100/src/main.ts
@@ -6,9 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 样式全局使用
+import 'vant/lib/index.css'
+import './styles/main.scss'
+
 const app = createApp(App)
-
-app.use(createPinia())
+app.use(createPinia)
 app.use(router)
-
 app.mount('#app')
