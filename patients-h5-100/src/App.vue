54aabae5a9d7d11ff3245481e22a52241129a07f
Yusong99 <yusongxu1999@gmail.com>
2024-03-07 23:21:34 +0800

    2024.3.7 23:21

--- patients-h5-100/src/App.vue
+++ patients-h5-100/src/App.vue
@@ -1,13 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from './stores/user'
+
+const store = useUserStore()
+</script>
 
 <template>
-  <!-- 验证vant颜色被覆盖 -->
-  <van-button type="primary">按钮</van-button>
-  <a href="#">123</a>
+  <p>{{ store.user }}</p>
+  <button @click="store.setUser({ id: '1', mobile: '1', account: '1', avatar: '1', token: '1' })">
+    登录
+  </button>
+  <button @click="store.delUser()">退出</button>
 </template>
-
-<style scoped lang="scss">
-a {
-  color: var(--cp-primary);
-}
-</style>
--- patients-h5-100/src/main.ts
+++ patients-h5-100/src/main.ts
@@ -5,12 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import persist from 'pinia-plugin-persistedstate'
 // 样式全局使用
 import 'vant/lib/index.css'
 import './styles/main.scss'
 
 const app = createApp(App)
-app.use(createPinia)
+app.use(createPinia().use(persist));
 app.use(router)
 app.mount('#app')
